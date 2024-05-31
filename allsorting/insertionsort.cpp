50b7e1295812cf7d8dd79c14c281cd32bf514e58
Md Furqan Hasan <115374363+Fh27@users.noreply.github.com>
2024-05-31 21:58:37 +0530

    Create insertionsort.cpp

---
+++ allsorting/insertionsort.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[]={7,8,1};
+    int n=3;
+    for(int i=0;i<n;i++){
+        int j=i;
+        while(j>0 && arr[j-1]>arr[j]){
+            swap(arr[j-1],arr[j]);
+            j--;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        cout<<arr[i]<<" ";
+    }
+    
+    
+    return 0;
+}
