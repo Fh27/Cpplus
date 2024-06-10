c9b11461968ca7945a150a9966a2fc7bfbc916d1
Md Furqan Hasan <115374363+Fh27@users.noreply.github.com>
2024-06-10 22:54:35 +0530

    Create Heights.cpp

---
+++ JuneLeetCode/Heights.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        int sum=0;
+        vector<int> ans;
+        ans=heights;
+       sort(ans.begin(),ans.end());
+        for(int i=0;i<heights.size();i++){
+            if(ans[i]!=heights[i]){
+                sum++;
+            }
+        }
+        return sum;
+    }
+};
