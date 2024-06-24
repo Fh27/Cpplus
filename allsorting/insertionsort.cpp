c99f814fe15d0d5b85b5f116a08f260da3473138
Md Furqan Hasan <115374363+Fh27@users.noreply.github.com>
2024-06-24 22:09:50 +0530

    Update insertionsort.cpp

--- allsorting/insertionsort.cpp
+++ allsorting/insertionsort.cpp
@@ -8,7 +8,8 @@ int main(){
         int j=i;
         while(j>0 && arr[j-1]>arr[j]){
             swap(arr[j-1],arr[j]);
-            j--;
+            
+j--;
         }
     }
     for (int i = 0; i < n; i++) {
