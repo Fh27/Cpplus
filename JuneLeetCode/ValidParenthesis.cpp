0c0c74e340ef5a2ad4dff4ea7abc70ec56fe409d
Md Furqan Hasan <115374363+Fh27@users.noreply.github.com>
2024-06-25 23:46:56 +0530

    Create ValidParenthesis.cpp

---
+++ JuneLeetCode/ValidParenthesis.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0;i<s.length();i++){
+            char ch=s[i];
+        if(ch=='(' || ch=='{' || ch=='['){
+st.push(ch);
+        }
+        else{
+if(!st.empty()){
+char tp=st.top();
+ if(ch==')' && tp=='('){
+    
+    st.pop();
+}
+else if(ch=='}' && tp=='{'){
+ 
+    st.pop();
+}
+else if(ch==']' && tp=='['){
+    st.pop();
+}
+else {
+    return false;
+}
+}else{
+    return false;
+}
+        }
+        }
+        if(st.empty()){
+            return true;
+        }
+        else 
+        return false;
+    } 
+};
