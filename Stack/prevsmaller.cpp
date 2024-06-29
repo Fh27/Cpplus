56fff2bfbed75f0eda94f07094d8541b6a9bc46e
Md Furqan Hasan <115374363+Fh27@users.noreply.github.com>
2024-06-29 19:54:03 +0530

    Create prevsmaller.cpp

---
+++ Stack/prevsmaller.cpp
@@ -0,0 +1,22 @@
+vector<int> prevSmallerElement(vector<int>& input) {
+        stack<int> s;
+        s.push(-1);
+        vector<int> ans(input.size());
+
+        //left to right -> prev smaller
+        for(int i=0; i<input.size(); i++) {
+                int curr = input[i];
+
+                //apka answer stack me 
+                while(s.top() != -1 && input[s.top()] >= curr) {
+                        s.pop();
+                }
+
+                //chotta element mil chuka h -> ans store
+                ans[i] = s.top();
+
+                //push krdo curr element ko
+                s.push(i);
+        }
+        return ans;
+}
