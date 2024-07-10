ba4c96e6921f077970c7e4886849784358edc46a
Md Furqan Hasan <115374363+Fh27@users.noreply.github.com>
2024-07-10 23:01:06 +0530

    Update Inordersuccesor.cpp

---
+++ BST/Inordersuccesor.cpp
@@ -0,0 +1,34 @@
+/*The structure of Node
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution{
+  public:
+    // returns the inorder successor of the Node x in BST (rooted at 'root')
+    Node * inOrderSuccessor(Node *root, Node *x)
+    {
+        //Your code here
+        Node* succ=0;
+        Node* curr=root;
+        while(curr){
+            if(curr->data>x->data){
+                succ=curr;
+                curr=curr->left;    
+            }
+            else{
+                curr=curr->right;
+            }
+        }
+        return succ;
+    }
+};
