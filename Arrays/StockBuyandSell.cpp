8b82c161e03e2e00a6b454b0107d6bd1f49ec170
Md Furqan Hasan <115374363+Fh27@users.noreply.github.com>
2024-07-30 21:17:32 +0530

    Create StockBuyandSell.cpp

---
+++ Arrays/StockBuyandSell.cpp
@@ -0,0 +1,10 @@
+ int maxProfit(vector<int>& prices) {
+        int maxi=0;
+        int mini=prices[0];
+        for( int i=1;i<prices.size();i++){
+            int cost=prices[i]-mini;
+            maxi=max(maxi,cost);
+            mini=min(mini,prices[i]);
+        }
+     return maxi ;
+    }
