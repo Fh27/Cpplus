3c49120219594f360a2702d2191ba323672c7146
Md Furqan Hasan <115374363+Fh27@users.noreply.github.com>
2024-08-19 19:11:31 +0530

    Create MinSizeSubArraySumK.cpp

---
+++ SlidingWindow/MinSizeSubArraySumK.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int i=0;
+        int j=0;
+        int len=INT_MAX;
+        int sum=0;
+        while(j<nums.size()){
+            sum=sum+nums[j];
+            while(sum>=target){
+                len=min(len,j-i+1);
+                sum=sum-nums[i];
+                i++;
+            }
+            j++;
+        }
+        if(len==INT_MAX){
+            return 0;
+        }
+        else{
+            return len;
+        }
+    }
+};
