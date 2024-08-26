e1c18a4f736268e3823ae9a62d71109dd9e8cd2a
Md Furqan Hasan <115374363+Fh27@users.noreply.github.com>
2024-08-27 02:56:53 +0530

    Create FirstOccuranceIndexString.cpp

---
+++ String/FirstOccuranceIndexString.cpp
@@ -0,0 +1,15 @@
+int strStr(string haystack, string needle) {
+        int n=haystack.size();
+        int m=needle.size();
+        for(int i=0;i<=n-m;i++){
+            for(int j=0;j<m;j++){
+                if(haystack[i+j]!=needle[j]){
+                    break;
+                }
+                if(j==m-1){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
