1dbc30cbcf93ddb88a63405f037d92f9aed54a22
Md Furqan Hasan <115374363+Fh27@users.noreply.github.com>
2024-08-31 01:38:54 +0530

    Create longestCommonPrefix.cpp

---
+++ String/longestCommonPrefix.cpp
@@ -0,0 +1,21 @@
+
+string longestCommonPrefix(vector<string>& strs) {
+        string ans="";
+        for(int i=0;i<strs[0].length();i++){
+            char ch=strs[0][i];
+            bool match=true;
+            for(int j=1;j<strs.size();j++){
+                if (strs[j].size() <i || ch!=strs[j][i]){
+                    match=false;
+                    break;
+                }
+            }
+                if(match==false){
+                    break;
+                }
+                else{
+                    ans.push_back(ch);
+                }   
+        }
+        return ans;
+ }
