ce3fde7f92c39641f1cb9abc5cd62eba26e04ba6
Md Furqan Hasan <115374363+Fh27@users.noreply.github.com>
2024-09-16 01:18:58 +0530

    Create longestCommonSubstr.cpp

---
+++ DynamicProgramming/Strings/longestCommonSubstr.cpp
@@ -0,0 +1,19 @@
+int longestCommonSubstr(string str1, string str2) {
+        // if matches add 1 and recursive call -- else directly zero
+        int n=str1.size();
+        int m=str2.size();
+        int maxi=0;
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int j=0;j<=m;j++) dp[0][j]=0;
+        for(int i=0;i<=n;i++) dp[i][0]=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(str1[i-1]==str2[j-1]){
+                    dp[i][j]= 1+dp[i-1][j-1];
+                    maxi=max(maxi,dp[i][j]);
+                }
+                else dp[i][j]=0;
+            }
+        }
+        return maxi;
+    }
