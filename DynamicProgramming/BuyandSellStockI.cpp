d02962d7cead7d671abbc2921635687dc8bc15b2
Md Furqan Hasan <115374363+Fh27@users.noreply.github.com>
2024-09-17 04:50:35 +0530

    Create BuyandSellStockI.cpp

---
+++ DynamicProgramming/BuyandSellStockI.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxi=0;
+        int mini=prices[0];
+        for(int i=1;i<prices.size();i++){
+            int cost=prices[i]-mini;
+            maxi=max(maxi,cost);
+            mini=min(mini,prices[i]);
+        }
+        return maxi;
+    }
+};
