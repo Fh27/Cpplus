a6d03eeb915ea7a0a98e864cfcb4aa34c416c517
Md Furqan Hasan <115374363+Fh27@users.noreply.github.com>
2024-10-12 23:45:14 +0530

    Update RotateImage.cpp

--- 2Darrays/RotateImage.cpp
+++ 2Darrays/RotateImage.cpp
@@ -4,7 +4,6 @@ public:
         for( int i=0;i<matrix.size()-1;i++){
             for( int j=i+1;j<matrix.size();j++){
                 swap(matrix[i][j],matrix[j][i]);
-
             }
         }
         for( int i=0;i<matrix.size();i++){
