ae0055233f857d8d790c02657501436828c903ed
Md Furqan Hasan <115374363+Fh27@users.noreply.github.com>
2025-05-03 17:20:13 +0530

    Update CopyRandomPtr.cpp

--- LinkedList/CopyRandomPtr.cpp
+++ LinkedList/CopyRandomPtr.cpp
@@ -16,34 +16,42 @@ public:
 
 class Solution {
 public:
+    void insert(Node* head){
+        Node* temp=head;
+        while(temp){
+            Node* copynode=new Node(temp->val);
+            copynode->next=temp->next;
+            temp->next=copynode;
+            temp=temp->next->next;
+        }
+       
+    }
+    void assignrondom(Node* head){
+        Node* temp=head;
+        while(temp){
+            Node* copynode=temp->next;
+            if(temp->random)copynode->random=temp->random->next;
+           else copynode->random=NULL;
+            temp=temp->next->next;
+        }
+    }
+    Node* detach(Node* head){
+        Node* dummy=new Node(-1);
+        Node* res=dummy;
+        Node* temp=head;
+        while(temp){
+            Node* copy=temp->next;
+            res->next=copy;
+            temp->next=temp->next->next;
+            res=res->next;
+            temp=temp->next;
+        }
+        return dummy->next;
+    }
     Node* copyRandomList(Node* head) {
-        if(!head) return 0;
-        //attach
-        Node* it=head;
-        while(it){
-            Node* newnode=new Node(it->val);
-            newnode->next=it->next;
-            it->next=newnode;
-            it=it->next->next;
-        }
-        //random pointer
-        it=head;
-        while(it){
-            Node*newnode=it->next;
-            newnode->random=it->random ?it->random->next : nullptr;
-            it=it->next->next;
-        }
-        //detach real and copy
-        it=head;
-        Node* cloneHead=it->next;
-        while(it){
-            Node*clone=it->next;
-            it->next=it->next->next;       //by mistake it was it onlyy but fixed it
-            if(clone->next){
-                clone->next=clone->next->next;
-            }
-            it=it->next;
-        }
-        return cloneHead;
+        if(!head) return head;
+        insert(head);
+        assignrondom(head);
+        return detach(head);
     }
 };
